b6c609ba104b1536a6684ab1d1f2a0c56b4ff189
Youssef Blaada <Youssef Blaada@192.168.1.101>
2019-05-17 19:31:39 +0000

    created repository for interaction entity

---
+++ project/src/main/java/com/shops/project/repositories/InteractionRepositoty.java
@@ -0,0 +1,12 @@
+package com.shops.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.shops.project.models.Interaction;
+
+
+@Repository
+public interface InteractionRepositoty extends JpaRepository<Interaction, Long>{
+
+}
