60cb84711466c3a7c6c41a21ded5763f5d6886a5
Youssef Blaada <Youssef Blaada@192.168.1.101>
2019-05-17 23:14:46 +0000

    SignIn & SignUp controller

---
+++ project/src/main/java/com/shops/project/controller/Authentication_Controller.java
@@ -0,0 +1,77 @@
+package com.shops.project.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+import com.shops.project.message.response.JwtResponse;
+import com.shops.project.message.response.ResponseMessage;
+import com.shops.project.models.User;
+import com.shops.project.repositories.UserRepository;
+import com.shops.project.security.jwt.JwtProvider;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/shop/auth")
+public class Authentication_Controller {
+	@Autowired
+	AuthenticationManager authenticationManager;  // Authentication Manager
+
+	@Autowired
+	UserRepository userRepository;  // User Repository
+
+	@Autowired
+	PasswordEncoder encoder;  // BCrypt Password Encoder
+
+	@Autowired
+	JwtProvider jwtProvider;  // JSON WEB TOKEN PROVIDER
+
+	
+	// @GetMapping used just for testing purpose will be replaced by @PostMapping
+	@GetMapping("/signin")
+	public ResponseEntity<?> authenticateUser(@RequestParam(value = "email") String email,
+			@RequestParam(value = "password") String password) {
+		
+		// User Authentication
+		Authentication authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(email, password));
+		
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		// Generating the JSON WEB TOKEN for the signed in user
+		String jwt = jwtProvider.generateJwtToken(authentication);
+		UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+		return ResponseEntity.ok(new JwtResponse(jwt, userDetails.getUsername()));
+	}
+
+	@GetMapping("/signup")
+	public ResponseEntity<?> registerUser(@RequestParam(value = "email") String email,
+			@RequestParam(value = "password") String password) {
+		
+		// Testing if the user is already signed up
+		if (userRepository.existsByEmail(email)) {
+			return new ResponseEntity<>(new ResponseMessage("Fail -> Email is already in use!"),
+					HttpStatus.BAD_REQUEST);
+		}
+
+		// Creating user's account
+		User user = new User(email, encoder.encode(password));
+		userRepository.save(user);
+		return new ResponseEntity<>(new ResponseMessage("User registered successfully!"), HttpStatus.OK);
+	}
+}
