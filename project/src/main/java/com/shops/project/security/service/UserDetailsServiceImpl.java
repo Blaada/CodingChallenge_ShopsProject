47ca7cd1a9e64ba7745b92a1d91753dc494be882
Youssef Blaada <Youssef Blaada@192.168.1.101>
2019-05-17 19:29:06 +0000

    Spring Security : user details

---
+++ project/src/main/java/com/shops/project/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.shops.project.security.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.shops.project.models.User;
+import com.shops.project.repositories.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+		User user = userRepository.findByEmail(email)
+				.orElseThrow(() -> new UsernameNotFoundException("User not found with email :" + email));
+		return UserPrinciple.build(user);
+	}
+
+}
