919aeaa0ab16e89096a16a0a6c7228bf8331544b
Youssef Blaada <Youssef Blaada@192.168.1.101>
2019-05-17 14:25:52 +0000

    Main

--- project/src/main/java/com/shops/project/ShopsProjectApplication.java
+++ project/src/main/java/com/shops/project/ShopsProjectApplication.java
@@ -6,10 +6,11 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+
 @ComponentScan(basePackages = { "com.shops.project.*" })
 @EntityScan("com.shops.project.models")  
 @EnableJpaRepositories(basePackages = "com.shops.project.repo")
+@SpringBootApplication
 public class ShopsProjectApplication {
 
 	public static void main(String[] args) {
