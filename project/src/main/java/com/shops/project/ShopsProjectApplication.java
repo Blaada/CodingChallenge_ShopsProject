23e4cea71cd482850b645095ad3806a5999f1e6c
Youssef Blaada <Youssef Blaada@192.168.1.101>
2019-05-17 19:21:07 +0000

    Fixed annotations

--- project/src/main/java/com/shops/project/ShopsProjectApplication.java
+++ project/src/main/java/com/shops/project/ShopsProjectApplication.java
@@ -7,10 +7,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
-@ComponentScan(basePackages = { "com.shops.project.*" })
-@EntityScan("com.shops.project.models")  
-@EnableJpaRepositories(basePackages = "com.shops.project.repo")
-@SpringBootApplication
+@ComponentScan(basePackages = { "com.shops.project.*" }) // Scanning the component of project 
+@EntityScan("com.shops.project.*")  // Scanning all entities 
+@EnableJpaRepositories(basePackages = "com.shops.project.repositories") // Enabling and Scanning the repositories
+@SpringBootApplication // Equivalent to using @Configuration, @EnableAutoConfiguration
 public class ShopsProjectApplication {
 
 	public static void main(String[] args) {
