0d4b8d3ab9eeb7c401d880f6314a11ea61cf4a9e
Youssef Blaada <Youssef Blaada@192.168.1.101>
2019-05-17 19:24:26 +0000

    Fixed User

--- project/src/main/java/com/shops/project/models/User.java
+++ project/src/main/java/com/shops/project/models/User.java
@@ -16,21 +16,12 @@ import javax.validation.constraints.Email;
 // User entity
 
 @Entity
-@Table(name = "users", uniqueConstraints = { @UniqueConstraint(columnNames = { "username" }),
-		@UniqueConstraint(columnNames = { "email" }) })
+@Table(name = "user", uniqueConstraints = {@UniqueConstraint(columnNames = { "email" }) })
 public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long Id;
-
-	@NotBlank
-	@Size(min = 5, max = 60)
-	private String name;     
-
-	@NotBlank
-	@Size(min = 5, max = 60)
-	private String username;
 
 	@NaturalId
 	@NotBlank
@@ -45,43 +36,24 @@ public class User {
 	public User() {
 
 	}
-	
+
 	// Fields constructor
 
-	public User(Long id, @NotBlank String name, @NotBlank String username, @NotBlank @Email String email,
+	public User(@NotBlank @Email String email,
 			@NotBlank String password) {
 		super();
-		Id = id;
-		this.name = name;
-		this.username = username;
 		this.email = email;
 		this.password = password;
 	}
-	
+
 	// Getters & Setters
-	
+
 	public Long getId() {
 		return Id;
 	}
 
 	public void setId(Long id) {
 		Id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
 	}
 
 	public String getEmail() {
@@ -99,7 +71,5 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	
 
 }
--- project/src/main/java/com/shops/project/repositories/UserRepository.java
+++ project/src/main/java/com/shops/project/repositories/UserRepository.java
@@ -1,10 +1,13 @@
 package com.shops.project.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.shops.project.models.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>{
-
+	Optional<User> findByEmail(String email);
+	Boolean existsByEmail(String email);
 }
