3063303c46d371c860b90582d7bf0627d00da7db
Youssef Blaada <Youssef Blaada@192.168.1.101>
2019-05-19 19:31:33 +0000

    Added query to specify shop list

--- project/src/main/java/com/shops/project/repositories/ShopRepository.java
+++ project/src/main/java/com/shops/project/repositories/ShopRepository.java
@@ -1,6 +1,10 @@
 package com.shops.project.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.shops.project.models.Shop;
@@ -8,5 +12,8 @@ import com.shops.project.models.Shop;
 
 @Repository
 public interface ShopRepository extends JpaRepository<Shop, Long>{
+	
+	@Query(value = "SELECT * FROM shop WHERE shop.shop_id NOT IN (SELECT shop_id FROM interaction WHERE interaction.user_id=:USER)",nativeQuery = true)
+	List<Shop> findAllShops(@Param("USER") Long user_id);
 
 }
